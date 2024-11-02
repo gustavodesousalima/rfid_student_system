f5c6865d18ba5582be5d981677fca1d9ce2d651b
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-11-02 03:27:01 -0300

    Update

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,9 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
 students = {}
 rfid_data = None  # Variável global para armazenar o ID lido
-DATA_FILE = 'C:/Users/Aluno/Desktop/db-rfid/alunos.json'  # Nome do arquivo para persistência
-id_admin = "FD 33 11 03"  # Defina o ID do administrador aqui
+DATA_FILE = 'C:/Users/gusta/OneDrive/Desktop/bd-rfid/alunos.json'  # Nome do arquivo para persistência
 
 def load_students():
     global students
@@ -50,21 +48,16 @@ def extract_uid(raw_data):
 def register_student():
     global rfid_data
     print("Aguardando leitura do cartão...")
-
     # Aguarda a leitura do cartão
     timeout = 10  # Tempo de espera em segundos
     start_time = time.time()
-
     # Aguarda o cartão ser lido por 10 segundos
     while rfid_data is None and (time.time() - start_time) < timeout:
         time.sleep(1)  # Espera 1 segundo antes de verificar novamente
-
     if rfid_data is None:
         return jsonify({"error": "ID do cartão RFID não lido"}), 400
-
     data = request.json
     name = data.get('name')
-
     if name:
         # Salva o aluno com o ID do cartão lido
         students[name] = rfid_data
@@ -80,58 +73,27 @@ def register_student():
 def access_student():
     global rfid_data
     print("Aguardando leitura do cartão...")
-
     # Aguarda a leitura do cartão
     timeout = 10  # Tempo de espera em segundos
     start_time = time.time()
-
     # Aguarda o cartão ser lido por 10 segundos
     while rfid_data is None and (time.time() - start_time) < timeout:
         time.sleep(1)  # Espera 1 segundo antes de verificar novamente
-
     if rfid_data is None:
         return jsonify({"error": "ID do cartão RFID não lido"}), 400
-
     # Verifica se o UID lido está no dicionário de alunos
     for name, uid in students.items():
         if uid == rfid_data:
             print(f"Acesso liberado para '{name}' com ID {rfid_data}")
             rfid_data = None  # Limpa o ID após a verificação
-            return jsonify({"message": f"Acesso liberado para aluno(a): {name}"}), 200
-
+            return jsonify({"message": f"Acesso liberado para '{name}'"}), 200
     rfid_data = None  # Limpa o ID após a verificação falha
     return jsonify({"error": "Acesso negado"}), 403
-
-@app.route('/access-admin', methods=['POST'])
-def access_admin():
-    global rfid_data
-    print("Aguardando leitura do cartão para acesso administrativo...")
-
-    # Aguarda a leitura do cartão
-    timeout = 10  # Tempo de espera em segundos
-    start_time = time.time()
-
-    # Aguarda o cartão ser lido por 10 segundos
-    while rfid_data is None and (time.time() - start_time) < timeout:
-        time.sleep(1)  # Espera 1 segundo antes de verificar novamente
-
-    if rfid_data is None:
-        return jsonify({"error": "ID do cartão RFID não lido"}), 400
-
-    # Verifica se o UID lido corresponde ao ID do administrador
-    if rfid_data == id_admin:
-        print(f"Acesso administrativo liberado com ID {rfid_data}")
-        rfid_data = None  # Limpa o ID após a verificação
-        return jsonify({"message": "Acesso administrativo liberado"}), 200
-    else:
-        print(f"Acesso administrativo negado com ID {rfid_data}")
-        rfid_data = None  # Limpa o ID após a verificação
-        return jsonify({"error": "Acesso administrativo negado"}), 403
 
 @app.route('/list', methods=['GET'])
 def list_students():
     return jsonify(students)
-
+    
 if __name__ == '__main__':
     load_students()  # Carrega os dados dos alunos no início
     threading.Thread(target=read_rfid, daemon=True).start()
